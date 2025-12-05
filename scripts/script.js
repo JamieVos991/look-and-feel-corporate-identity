44ccb75af6df3082c5f84715453b777e0c304dd6
Jamie Vos <Jamievos100@gmail.com>
2025-12-05 09:35:23 +0100

    Javascript interactie

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const hamburger = document.getElementById("hamburger");
-    const closeButton = document.getElementById("close");
-    const mobileMenu = document.getElementById("mobile-menu");
-    const menuBg = document.querySelector(".menu-bg");
-    const body = document.body;
+    // 1. Elementen selecteren
+    let hamburgerKnop = document.getElementById("hamburger");
+    let sluitKnop = document.getElementById("sluiten");
+    let mobielMenu = document.getElementById("mobiel-menu");
+    let MenuAchtergrond = document.querySelector(".menu-achtergrond");
   
-    hamburger.addEventListener("click", () => {
-      mobileMenu.classList.remove("hidden");
-      menuBg.classList.add("active");
-      body.classList.add("no-scroll");
+    // 2. Wachten totdat iemand erop klikt
+    hamburgerKnop.addEventListener("click", () => {
+
+      // 3. Classes worden toegevoegd / verwijderd
+      mobielMenu.classList.remove("verbergen");
+      MenuAchtergrond.classList.add("actief");
+      document.body.classList.add("geen-scroll");
     });
   
-    closeButton.addEventListener("click", () => {
-      mobileMenu.classList.add("hidden");
-      menuBg.classList.remove("active");
-      body.classList.remove("no-scroll");
+    // 2. Wachten totdat iemand erop klikt
+    sluitKnop.addEventListener("click", () => {
+
+      // 3. Classes worden toegevoegd / verwijderd
+      mobielMenu.classList.add("verbergen");
+      MenuAchtergrond.classList.remove("actief");
+      document.body.classList.remove("geen-scroll");
     });
-  
-    menuBg.addEventListener("click", () => {
-      mobileMenu.classList.add("hidden");
-      menuBg.classList.remove("active");
-      body.classList.remove("no-scroll");
-    });
-  });
-  
